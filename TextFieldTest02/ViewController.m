8fee74466395be6a0147512c4c13a494687748e0
Chae Yungi <l8673901@gmail.com>
2014-01-03 18:10:24 +0900

    키보드 테스트 두번째 실습입니다.
    z 입력 방지 실습입니다.

--- TextFieldTest02/ViewController.m
+++ TextFieldTest02/ViewController.m
@@ -18,6 +18,41 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 205, 50, 50)];
+	label.text = @"z입력금지";
+//	label.textColor = [UIColor whiteColor];
+//	label.backgroundColor = [UIColor clearColor];
+	[label sizeToFit];
+	[self.view addSubview:label];
+	
+	UITextField *tf = [[UITextField alloc] initWithFrame:CGRectMake(130, 200, 171, 31)];
+	tf.borderStyle = UITextBorderStyleRoundedRect;
+	tf.delegate = self;
+	tf.tag = 3;
+	
+	[self.view addSubview:tf];
+	
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+	if(3==textField.tag && NSOrderedSame == [string compare:@"z" options:NSCaseInsensitiveSearch])
+		return NO;
+	return YES;
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+	if(2==textField.tag)
+		return NO;
+	return YES;
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	NSLog(@"Input : %@", textField.text);
+	[textField resignFirstResponder];
+	return YES;
+	
 }
 
 - (void)didReceiveMemoryWarning
